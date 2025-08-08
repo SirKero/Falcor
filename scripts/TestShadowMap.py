703785633db2511328c939819d0f281ae7a5d5b1
SirKero <sirkero@gmx.de>
2025-08-08 11:28:40 +0200

    Changed dlss order in scripts

--- scripts/TestShadowMap.py
+++ scripts/TestShadowMap.py
@@ -6,9 +6,13 @@ def render_graph_DefaultRenderGraph():
     g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True})
     g.create_pass('TestShadowMap', 'TestShadowMap', {'computeDirect': True, 'shadowTracingMode': 'ShadowMap'})
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
+    g.create_pass('DLSSPass', 'DLSSPass', {'enabled': True, 'outputSize': 'Default', 'profile': 'DLAA', 'preset': 'Default(CNN)', 'motionVectorScale': 'Relative', 'isHDR': True, 'useJitteredMV': False, 'sharpness': 0.0, 'exposure': 0.0})
     g.add_edge('VBufferRT.vbuffer', 'TestShadowMap.vbuffer')
     g.add_edge('VBufferRT.viewW', 'TestShadowMap.viewW')
-    g.add_edge('TestShadowMap.color', 'ToneMapper.src')
+    g.add_edge('TestShadowMap.color', 'DLSSPass.color')
+    g.add_edge('VBufferRT.depth', 'DLSSPass.depth')
+    g.add_edge('VBufferRT.mvec', 'DLSSPass.mvec')
+    g.add_edge('DLSSPass.output', 'ToneMapper.src')
     g.mark_output('ToneMapper.dst')
     return g
 
