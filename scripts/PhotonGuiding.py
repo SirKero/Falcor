b0fb2e09ccf7eaf058f1f59b784fd0ba895f4af1
SirKero <sirkero@gmx.de>
2025-10-08 17:37:28 +0200

    Added VideoRecorder Pass

--- scripts/PhotonGuiding.py
+++ scripts/PhotonGuiding.py
@@ -7,11 +7,13 @@ def render_graph_PhotonGuiding():
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
     g.create_pass('PhotonGuiding', 'PhotonGuiding', {})
     g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'cullNonOpaque': False, 'useTraceRayInline': False, 'useDOF': True})
+    g.create_pass('VideoRecorder', 'VideoRecorder', {})
     g.add_edge('AccumulatePass.output', 'ToneMapper.src')
     g.add_edge('PhotonGuiding.ColorOut', 'AccumulatePass.input')
     g.add_edge('VBufferRT.vbuffer', 'PhotonGuiding.VBuffer')
     g.add_edge('VBufferRT.viewW', 'PhotonGuiding.View')
     g.add_edge('VBufferRT.mvec', 'PhotonGuiding.MotionVector')
+    g.add_edge('VideoRecorder', 'VBufferRT')
     g.mark_output('ToneMapper.dst')
     g.mark_output('AccumulatePass.output')
     return g
